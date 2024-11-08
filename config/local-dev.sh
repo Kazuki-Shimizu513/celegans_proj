77b1d3b2c61af85235907833731c8225e586c4fb
Kazuki Shimizu <kazukimacbook19980513@gmail.com>
2024-11-08 13:27:59 +0900

    local-dev

--- config/local-dev.sh
+++ config/local-dev.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
 python ./src/celegans_proj/run/run.py \
-  --exp_name exp_example \
-
+  --exp_name exp_20241106 \
   --in_dir /mnt/e/WDDD2_AD \
   --out_dir  /mnt/c/Users/compbio/Desktop/shimizudata/ \
-
   --batch 1 \
-  --resolution 128 \
-
+  --resolution 256 \
   --model_names \
               "Patchcore" \
               "ReverseDistillation" \
-
   --pseudo_anomaly_modes  \
               "wildType" \
               "patchBlack" \
