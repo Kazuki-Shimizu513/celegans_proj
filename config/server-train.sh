94e8a87d43cfaa30f7484df601bfda27a0b06544
Kazuki Shimizu <kazukimacbook19980513@gmail.com>
2024-11-29 02:35:03 +0000

    server run file

---
+++ config/server-train.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+python ./src/celegans_proj/run/run.py \
+  --exp_name exp_20241129 \
+  --in_dir /mnt/e/WDDD2_AD \
+  --out_dir  /mnt/c/Users/compbio/Desktop/shimizudata/ \
+  --batch 1 \
+  --resolution 256 \
+  --model_names \
+          "MyModel"\
+  --pseudo_anomaly_modes  \
+              "wildType" \
+              "patchBlack" \
+              "gridBlack" \
+              "zoom" \
+              "shrink" \
+              "oneCell" \
+  --anomaly_gene_list \
+              "wildType" \
+              "F10E9.8" \
+              "F54E7.3" \
+
+
+
+
+
